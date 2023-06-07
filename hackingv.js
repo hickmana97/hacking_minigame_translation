a6cdcd0ef632dcaed430e06d793624727bb07f32
hickmananwslc <132064235+hickmananwslc@users.noreply.github.com>
2023-06-07 13:41:51 +0100

    Update hackingv.js
    Added password imbedding functionality, had to adjust password selection with a while loop, saving before testing in case of a crash

--- hackingv.js
+++ hackingv.js
@@ -2,7 +2,7 @@ function setup() {
   createCanvas(600, 500);
   background('black');
   textFont('Courier New');
-  textSize(20);
+  textSize(17);
   stroke(2, 216, 24);
   strokeWeight(1);
   fill(2, 216, 24);
@@ -11,6 +11,7 @@ function setup() {
 }
 
 function draw(){
+  noLoop()
   game.play();
 }
 
@@ -20,14 +21,14 @@ class Game{
     this.attemptsLeft = 4;
     this.location = [10, 20];
     this.textOffset = textSize() * 1.25;
-    this.passwordList = passwordList.generatePasswordList()
-    this.passwordList = passwordList.passwordsGenerated
+    
 
   }
   
   play(){
     this.displayHeader();
-    console.log(this.passwordList)
+    this.displayPasswordList();
+    
     
   } 
   
@@ -37,13 +38,18 @@ class Game{
   }
   
   displayLine(string){
-    loop();
     for(let item of string){
       text(item, this.location[0], this.location[1]);
       this.location[1] = this.location[1] + this.textOffset;
     }
-    this.location[1] = this.location[1] + this.textOffset;
-    noLoop();
+    redraw()
+
+  }
+  
+  displayPasswordList(){
+    this.passwordList = passwordList.generatePasswordList()
+    this.passwordList = passwordList.passwordsGenerated
+    this.displayLine(this.passwordList)
   }
   
 }
@@ -56,15 +62,39 @@ class Passwords{
   generatePasswordList(){
     this.samplePasswords.push('PROVIDE', 'SETTING', 'CANTINA', 'CUTTING', 'HUNTERS', 'SURVIVE',  'HEARING', 'HUNTING', 'REALIZE', 'NOTHING', 'OVERLAP', 'FINDING', 'PUTTING', 'NURTURE', 'RELIEVE', 'DESTROY', 'HABITAT', 'ICEBERG', 'VACCINE', 'VACANCY', 'ABIDING', 'ABILITY');
     for(; this.passwordsGenerated.length < 13;){
-      let selection = random(this.samplePasswords);
-      if(this.passwordsGenerated.includes(selection)){
-        let selection = random(this.samplePasswords);
+      this.selection = random(this.samplePasswords);
+      while(this.passwordsGenerated.includes(this.selection)){
+        this.selection = random(this.samplePasswords)
       }
-      else{
-      this.passwordsGenerated.push(selection)
-      }
+      /*if(this.passwordsGenerated.includes(this.selection)){
+        this.selection = random(this.samplePasswords);
+      }*/
+      this.embedSelection = this.embedPasswordList(this.selection, 20);
+      this.passwordsGenerated.push(this.embedSelection);
+      
     }
-    this.passwordsGenerated.push('')
+    this.passwordsGenerated.push('');
+
+  }
+  
+  embedPasswordList(word, size){
+    this.filler = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '~', '[', ']', '{', '}'];
+    this.embedding = [];
+    this.wordLength = word.length;
+    this.splitIndex = floor(random(0, size - this.wordLength));
+    for(let i = 0; i < this.splitIndex + 1; i++){
+      this.embedding.push(random(this.filler));
+    }
+    
+    this.embedding = this.embedding.concat(word);
+    
+    for(let i = this.wordLength + this.splitIndex; i < size - 1; i++){
+      this.embedding.push(random(this.filler));
+    }
+  
+    this.embeddedPassword = this.embedding.join('');
+    console.log(this.embeddedPassword);
+    console.log(this.embeddedPassword.length);
 
   }
   
